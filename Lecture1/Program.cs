369d21ab42bebfc8767e56c355b40e4b5930b1f0
Diana Mesniankina <dmesniankina@yandex.ru>
2024-06-06 17:53:31 +0200

    add to lecture1

--- Lecture1/Program.cs
+++ Lecture1/Program.cs
@@ -56,5 +56,33 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-// System.Console.WriteLine($"Ширина: {Console.BufferWidth}, Высота: {Console.BufferHeight}");
+// // System.Console.WriteLine($"Ширина: {Console.BufferWidth}, Высота: {Console.BufferHeight}");
 
+
+
+int x = xa, y = xb;
+int count = 0;
+while (count < 1000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1;
+
+}
