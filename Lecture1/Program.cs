e0adb87355976ec2ab1e89eb1a72762b9fc7799c
Diana Mesniankina <dmesniankina@yandex.ru>
2024-06-06 17:25:56 +0200

    lecture1

---
+++ Lecture1/Program.cs
@@ -0,0 +1,60 @@
+﻿// int numberA = new Random().Next(1, 10);
+// Console.WriteLine(numberA);
+// int numberB = new Random().Next(1,10);
+// Console.WriteLine(numberB);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+
+
+// Console.Write("Введите имя пользователя: ");
+// string username = Console.ReadLine();
+// if (username.ToLower() == "disha")
+// {
+//     Console.WriteLine("Привет, Disha!!!");
+// }
+// else
+// {
+//     Console.Write("Привет, ");
+//     Console.WriteLine(username);
+// }
+
+
+
+// int a = 1;
+// int b = 2;
+// int c = 6;
+// int d = 8;
+// int e = 15;
+
+// int max = a;
+
+// if (a>max) max = a;
+// if(b>max) max = b;
+// if(c>max) max = c;
+// if(d>max) max = d;
+// if(e>max) max = e;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+
+
+using System.Runtime.InteropServices;
+
+Console.Clear();
+int xa = 1, ya = 1,
+    xb = 1, yb = 7,
+    xc = 30, yc = 7;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+// System.Console.WriteLine($"Ширина: {Console.BufferWidth}, Высота: {Console.BufferHeight}");
+
