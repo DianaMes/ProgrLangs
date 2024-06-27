6feef96dcd552c1292384939053fd1c49d4e7fc5
Diana Mesniankina <dmesniankina@yandex.ru>
2024-06-27 15:06:38 +0200

    Lecture5

---
+++ Lecture5/Program.cs
@@ -0,0 +1,29 @@
+﻿// void OpenMatryoshka(int size)
+// {
+//     Console.WriteLine($"Opening matryoshka of size {size}...");
+//     if (size == 1)
+//     {
+//         Console.WriteLine("Reached the smallest matryoshka!");
+//         return;
+//     }
+//     OpenMatryoshka(size - 1);
+// }
+
+// OpenMatryoshka(5);
+
+// _____________________________________________________________________
+
+// Задача 1:
+// Вычислить факториал от натур. числа N
+
+int Fact(int n)
+{
+    if(n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+Console.Write(Fact(5));
